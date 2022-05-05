647503989b0fac3e72074fbef80bf385ae58c2f0
pfistfl <pfistererf@googlemail.com>
2022-05-05 09:21:49 +0200

    update code for glmnet

--- code/PipeOpFlatFunct.R
+++ code/PipeOpFlatFunct.R
@@ -11,7 +11,7 @@
 #'   See [`Selector`] for example functions. Default is selector_type("functional")`.
 #'   All features selected by this selector must be of type functional, otherwise an error is cast.
 #' @export
-PipeOpFlatFunct = R6Class("PipeOpFlatFunct",
+PipeOpFlatFunct = R6::R6Class("PipeOpFlatFunct",
   inherit = mlr3pipelines::PipeOpTaskPreprocSimple,
   public = list(
     initialize = function(id = "flatfunct", param_vals = list()) {
@@ -27,7 +27,7 @@ PipeOpFlatFunct = R6Class("PipeOpFlatFunct",
         id = id,
         param_set = ps(),
         param_vals = param_vals,
-        packages = c("mlr3fda", "mlr3pipelines"),
+        packages = c("mlr3pipelines"),
         feature_types = "functional"
       )
     }
@@ -38,27 +38,37 @@ PipeOpFlatFunct = R6Class("PipeOpFlatFunct",
       if (!length(cols)) {
         return(task)
       }
-      dt = task$data(cols = cols)
+      data = task$data(cols = cols)
 
-      # TODO: check that there are no name clashes
+      d_flat = data.table(do.call("cbind", imap(data, function(x,nm) {
+          x = do.call("rbind", map(x, 1))
+          colnames(x) = paste0(nm, ".", seq_len(ncol(x)))
+          return(x)
+        })))
 
-      # TODO: to be save we should write the .transform function (and not transform_dt), because
-      # we cannot ensure that we don't have name-clashes with the original data.table
-      flattened = imap(dt,
-        function(x, nm) {
-          flat = flatten_functional(x)
-          d = as.data.table(flat)
-          d
-        }
-      )
-      # convert to data.table and append names
-      dt_flat = invoke(cbind, .args = flattened)
-
-      task$select(setdiff(task$feature_names, cols))$cbind(dt_flat)
+      task$select(setdiff(task$feature_names, cols))$cbind(d_flat)
     }
   )
 )
 
+
+    flatten_functional = function(x) {
+      assert_class(x, "functional")
+      args = unlist(map(x, "arg"))
+      args = unique(args)
+      args = sort(args)
+
+      m = matrix(nrow = length(x), ncol = length(args))
+      colnames(m) = args
+      for (i in seq_along(x)) {
+        idx = match(x[[i]]$arg, args)
+        m[i, idx] = x[[i]]$value
+      }
+      return(m)
+    }
+    
+mlr_pipeops = utils::getFromNamespace("mlr_pipeops", ns = "mlr3pipelines")
+mlr_pipeops$add("flatfunct", PipeOpFlatFunct)
 #' `private$.get_state` must not change its input value in-place and must return
 #' something that will be written into `$state`
 #' (which must not be NULL), `private$.transform()` should modify its argument in-place;
--- code/benchmark.R
+++ code/benchmark.R
@@ -1,5 +1,5 @@
 # devtools::install_github("https://github.com/mlr-org/mlr3tuningspaces")
-reticulate::use_condaenv("deepregression")
+# reticulate::use_condaenv("deepregression")
 library(reticulate)
 library(mlr3keras)
 library(mlr3misc)
@@ -12,8 +12,11 @@ library(dplyr)
 library(mlr3tuning)
 library(mlr3hyperband)
 library(mlr3tuningspaces)
+library(mlr3pipelines)
+library(mlr3learners)
 source("code/mlr3keras.R")
 source("code/xgboost.R")
+source("code/PipeOpFlatFunct.R")
 if(file.exists(".RData")) file.remove(".RData")
 
 #---------------------------------------------------------------------
@@ -33,16 +36,15 @@ map(names(gr), function(x) {set(df, j = x, value = gr[[x]]); NULL})
 
 gait = TaskClassif$new("gait", df, target = "grp")
 
-
-validation_run <- FALSE
 #---------------------------------------------------------------------
-# Switch to false to run real experimentvalidation_run = FALSE
+# Switch to false to run real experiment
+validation_run = TRUE
 seed = 123456L
 if (validation_run) {
   max_epochs = 4L
   sample_rows = c(2187L, 2218L, 2080L, 533L, 328L,329L:332L, 152L, 1825L, 2294L, 617L, 533L, 2295L, 396L, 1448L, 532L, 378L, 675L, 878L, 2L, 830L, 207L, 38L, 15L, 1406L, 1279L, 1532L, 1L)
 } else {
-  max_epochs = 250L
+  max_epochs = 150L
   sample_rows = gait$row_ids
 }
 
@@ -56,6 +58,7 @@ inception = LearnerClassifKerasFDAInception$new(id = "inception", architecture =
 fcnet = LearnerClassifKerasFDAFCN$new(id = "fcnet", architecture = KerasArchitectureFCN$new())
 
 xgboost = LearnerClassifXgboostFDA$new()
+
 
 #----------------------------------------------------------------------
 # Global Tuning settings
@@ -142,23 +145,8 @@ inception_at = AutoTuner$new(
 
 # -------------------------- Set Up XGBOOST ------------------------
 tune_space_xgb = mlr3tuningspaces::lts("classif.xgboost.default")
-aug_space = list(
-  augmentation_ratio = to_tune(0, 2),
-  jitter = to_tune(),
-  scaling = to_tune(),
-  permutation = to_tune(),
-  randompermutation = to_tune(),
-  magwarp = to_tune(),
-  timewarp = to_tune(),
-  windowslice = to_tune(),
-  windowwarp = to_tune(),
-  # rotation = to_tune(),
-  spawner = to_tune(),
-  dtwwarp = to_tune()
-)
 tune_space_xgb$values$nrounds = to_tune(p_int(lower = 1, upper = max_epochs, tags = "budget"))
-xgboost$param_set$values = insert_named(xgboost$param_set$values, c(aug_space, tune_space_xgb$values)) # comment out for default
-
+xgboost$param_set$values = insert_named(xgboost$param_set$values, tune_space_xgb$values)
 tuner = tnr("hyperband")
 
 xgb_at = AutoTuner$new(
@@ -173,109 +161,92 @@ xgb_at = AutoTuner$new(
 )
 
 
-xgb_at$train(gait)
+# -------------------------- Set Up LOG_REG ------------------------
+
+logreg = as_learner( po("flatfunct") %>>% po("learner", lrn("classif.glmnet"), lambda = 0) )
 
 # -------------------------- Train ------------------------
 set.seed(seed)
 learners <- list (fcnet_at, inception_at, xgb_at)
 
+if (FALSE) {
 design <- benchmark_grid(tasks = gait$clone()$filter(rows = sample_rows),
                          learners = learners,
-                         resamplings = resampling)
+                         resamplings = rsmp("holdout", ratio = 0.8))
 
-# task, resample, xgboost learner
+bmr <- benchmark(design,
+                 store_models = TRUE,
+                 store_backends = FALSE,
+                 encapsulate = "none")
+}
 
-# DR: commented out
-# bmr <- benchmark(design,
-#                  store_models = TRUE,
-#                  store_backends = FALSE,
-#                  encapsulate = "none")
-# 
-# saveRDS(bmr, file="output/resampling_models.RDS")
-# 
-bmr <- readRDS("output/resampling_models.RDS")
+# source("code/benchmark.R")
 
-measures <- list (msr("classif.acc"),
-                  msr("classif.bacc"))
+# res <- list (msr("classif.acc"),
+#                   msr("classif.bacc"))
 
-resample_perf <- as.data.table (bmr$score(measures = measures)) %>%
-  as.data.frame() %>%
-  dplyr::select (nr, task_id, learner_id, resampling_id, iteration, matches ("classif."))
+# resample_perf <- as.data.table (bmr$score(measures = measures)) %>%
+#   as.data.frame() %>%
+#   dplyr::select (nr, task_id, learner_id, resampling_id, iteration, matches ("classif."))
 
-# 
+
 # saveRDS(resample_perf,
 #         "output/resample_perf.RDS")
 # # resample_perf <- readRDS("output/resample_perf.RDS")
-# 
-resample_perf
 
-aggr = bmr$aggregate() # bmr$score()
-print(aggr)
+# saveRDS(bmr,
+#         "output/resampling_models.RDS")
+# # bmr <- readRDS("output/resampling_models.RDS")
 
-learners = as.data.table(bmr)$learner
-best_tuning_res <- lapply(1:length(learners), function(i) learners[[i]]$tuning_result)
 
-tune_res <- extract_inner_tuning_results(bmr)
-# 
-# 
-# # hyperband_schedule = function(r_min, r_max, eta, integer_budget = TRUE) {
-# #   r = r_max / r_min
-# #   s_max = floor(log(r, eta))
-# #   b = (s_max + 1) * r
-# 
-# #   map_dtr(s_max:0, function(s) {
-# #     nb = ceiling((b / r) * ((eta^s) / (s + 1)))
-# #     rb = r * eta^(-s)
-# #     map_dtr(0:s, function(i) {
-# #       ni = floor(nb * eta^(-i))
-# #       ri = r_min * rb * eta^i
-# #       if (integer_budget) ri = round(ri)
-# #       data.table(bracket = s, stage = i, budget = ri, n = ni)
-# #     })
-# #   })
-# # }
-# 
-# # hyperband_schedule(1, 250, 2)
-# 
-# # bmr$resample_results$resample_result[[1]]
-# 
-# 
-# inception_default$lr <- exp(inception_default$lr)
-# fcnet_default$lr <- exp(fcnet_default$lr)
-# inception_default$epochs <- 300
-# fcnet_default$epochs <- 300
-# 
-# # # No augmentation
+# resample_perf
+
+# aggr = bmr$aggregate()
+# print(aggr)
+
+# learners = as.data.table(bmr)$learner
+# lapply(1:length(learners), function(i) learners[[i]]$tuning_result)
+
+# tune_res <- extract_inner_tuning_results(bmr)
+
+
+# hyperband_schedule = function(r_min, r_max, eta, integer_budget = TRUE) {
+#   r = r_max / r_min
+#   s_max = floor(log(r, eta))
+#   b = (s_max + 1) * r
+
+#   map_dtr(s_max:0, function(s) {
+#     nb = ceiling((b / r) * ((eta^s) / (s + 1)))
+#     rb = r * eta^(-s)
+#     map_dtr(0:s, function(i) {
+#       ni = floor(nb * eta^(-i))
+#       ri = r_min * rb * eta^i
+#       if (integer_budget) ri = round(ri)
+#       data.table(bracket = s, stage = i, budget = ri, n = ni)
+#     })
+#   })
+# }
+
+# hyperband_schedule(1, 250, 2)
+
+# bmr$resample_results$resample_result[[1]]
+
+
+# # No augmentation
 # inception$param_set$values = insert_named(inception$param_set$values, inception_default)
 # fcnet$param_set$values = insert_named(fcnet$param_set$values, fcnet_default)
-# 
+
 # noaug_design = benchmark_grid(
 #   tasks = gait,
 #   learners = c(inception, fcnet),
 #   resamplings = bmr$resamplings$resampling
 # )
-# 
+
 # bmr2 = benchmark(noaug_design)
 # saveRDS(bmr2, file="output/resampling_models_noaug.RDS")
-# 
+
 # aggr2 = bmr2$aggregate()
 # print(aggr2)
 # sampler_fcnet$sample(1)
 # sampler_inception$sample(1)
-
-############# Re-run best setting ##############
-library(mlr3misc)
-
-params <- as.list(as.data.frame(best_tuning_res[[2]])[,-1*c(18:20)])
-params$lr <- exp(params$lr)
-
-inception$param_set$values <- 
-  insert_named(inception$param_set$values, params)
-
-inception$param_set$values$epochs <- 1000L
-inception$param_set$values$val_idx <- bmr$resamplings$resampling[[1]]$test_set(1)
-
-inception$train(gait)
-
-# rerun_res <- resample(gait, inception, bmr$resamplings$resampling[[1]])
 
